5d5515010e5542956fd28934bb27e7ed5cd119ef
Kevin Selwyn <kevinselwyn@gmail.com>
2016-05-04 16:46:22 -0400

    updates + cleanup

--- img2chr.py
+++ img2chr.py
@@ -1,33 +1,38 @@
 #!/usr/bin/python
+"""img2chr"""
 
 import math
-import os
 import sys
+import argparse
 from PIL import Image
 
-colors = [0, 0x55, 0xAA, 0xFF]
-extensions = ('.png', '.jpg', '.jpeg', '.gif', '.bmp')
-
+COLORS = [0, 0x55, 0xAA, 0xFF]
+EXTENSIONS = ('.png', '.jpg', '.jpeg', '.gif', '.bmp')
 
 def load_img(filename):
+    """Loads image"""
+
     img = Image.open(filename)
 
     return img
 
-
 def save_chr(filename, data):
+    """Saves chr"""
+
     with open(filename, 'wb') as f:
         f.write(data)
 
-
 def get_color(pixel):
+    """Gets normalized color of pixel"""
+
     rgb = math.floor((pixel[0] + pixel[1] + pixel[2]) / 3)
-    color = colors.index(min(colors, key=lambda x: abs(x - rgb)))
+    color = COLORS.index(min(COLORS, key=lambda x: abs(x - rgb)))
 
     return color
 
-
 def str2bin(string):
+    """Converts binary string to binary int"""
+
     output = bytearray()
 
     for x in range(0, len(string), 8):
@@ -35,8 +40,9 @@ def str2bin(string):
 
     return output
 
-
 def img2bin(img):
+    """Converts image to binary"""
+
     width, height = img.size
     pixel = img.load()
     color = 0
@@ -57,28 +63,25 @@ def img2bin(img):
 
     return binary
 
-
-def usage(program=os.path.basename(__file__)):
-    print 'python %s <img>' % program
-
-
 def main(argc, argv):
-    if argc < 2 or argv[1] in ('-h', '--help'):
-        usage(argv[0])
-        sys.exit(1)
+    """Main function"""
 
-    input_filename = argv[1]
+    parser = argparse.ArgumentParser(description='Converts images to the NES CHR format for programming NES ROMs in 6502 assembly')
+    parser.add_argument('img', action='store')
+
+    args = parser.parse_args(argv[1:argc])
+
+    input_filename = args.img
     output_filename = input_filename.rsplit('.', 1)[0] + '.chr'
 
-    if input_filename.endswith(extensions) != True:
-        raise Exception('Invalid image extension')
+    if input_filename.endswith(EXTENSIONS) != True:
+        print 'Invalid image extension'
         sys.exit(1)
 
     img = load_img(input_filename)
     binary = img2bin(img)
     output = str2bin(binary)
     save_chr(output_filename, output)
-
 
 if __name__ == '__main__':
     main(len(sys.argv), sys.argv)
